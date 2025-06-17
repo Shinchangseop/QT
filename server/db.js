cd11e2bf011d0e6a3468bbf451d2153f29dac494
Shinchangseop <sku201908@gmail.com>
2025-06-17 13:26:49 +0900

    FIANL 01

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 const client = new Client({
-  user: 'postgres',         // PostgreSQL 사용자명
-  host: 'localhost',        // PostgreSQL 호스트
-  database: 'qtdb',         // 데이터베이스 이름을 'qtdb'로 변경
-  password: 'hoonz@001105', // 비밀번호
-  port: 5432,               // PostgreSQL 기본 포트
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 client.connect()
